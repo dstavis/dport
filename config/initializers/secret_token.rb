cbc1c4666e7577d7958c4993d80a828896ae2e08
David Stavis <davidstavis@gmail.com>
2014-07-25 19:16:28 -0700

    boilerplate rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dport::Application.config.secret_token = '06fe6599263d836b1e453bc62532f0c1a7a86e7dff80c2de93f4b6b89f69df3279402d5a4e33ed2be857bf961ca45d496592b49a24f8aeeaa8dd97b8c320c92b'
